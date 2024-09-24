a269e8f0d70ff241acd3af9eb867701dff27112f
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-09-24 01:45:38 +0000

    update

--- runtimeChange.sh
+++ runtimeChange.sh
@@ -22,22 +22,5 @@ curl -X POST "$DT_URL_OBSLAB_LOG_PROBLEM_DETECTION/api/v2/events/ingest" \
     }
   }"
 
-##############
-# Step 2
-# Change the $2 feature flag key to the value of $3
-# This is messy (and in reality would be handled with a proper GitOps process eg. Pull Requests and a tool like ArgoCD)
-# 1. Read flags.yaml with yq
-# 2. Get the JSON flag values from .data and pass to jq
-# 3. jq changes the defaultValue
-# 4. This is saved as an environment variable RESULT
-# 5. Add a pipe and newline to result and use yq to edit flags.yaml inplace
-# 6. kubectl apply the changed file
-##############
-RESULT=$(yq '.data["demo.flagd.json"]' .devcontainer/otel-demo/flags.yaml | yq ".flags.$2.defaultVariant = \"$3\"")
-res=$RESULT yq -i '.data["demo.flagd.json"] = env(res)' .devcontainer/otel-demo/flags.yaml
-sed -i 's/  demo.flagd.json: /  demo.flagd.json: |\n    /g' .devcontainer/otel-demo/flags.yaml
 
-# Apply changes
-kubectl apply -f .devcontainer/otel-demo/flags.yaml
-
-echo "service: $1 flag: $2 property: defaultVariant set to value: $3"
+echo "Now manually change cartServiceFailure.defaultValue to on in .devcontainer/otel-demo/flags.yaml"
