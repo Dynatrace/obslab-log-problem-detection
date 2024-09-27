b1dfe555038d3adab7be87c42b4421380eb5057f
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-09-27 04:29:23 +0000

    update URL building

--- runtimeChange.sh
+++ runtimeChange.sh
@@ -4,12 +4,32 @@
 
 echo "Changing feature flag key: $2 to $3 for service: $1"
 
+# Build URL from parts
+full_apps_url="https://${DT_ENV_ID_OBSLAB_LOG_PROBLEM_DETECTION}"
+full_gen2_url=""
+
+if [ "${DT_ENV_OBSLAB_LOG_PROBLEM_DETECTION}" = "dev" ]; then
+  echo "environment is dev"
+  full_apps_url+=".dev.apps.dynatracelabs.com"
+  # Remove apps.
+  full_gen2_url=${full_apps_url/apps.}
+elif [ "${DT_ENV_OBSLAB_LOG_PROBLEM_DETECTION}" = "sprint" ]; then
+  echo "environment is sprint"
+  full_apps_url+=".sprint.apps.dynatracelabs.com"
+  # Remove apps.
+  full_gen2_url=${full_apps_url/apps.}
+else
+  echo "DT_ENV_OBSLAB_LOG_PROBLEM_DETECTION is either 'live' or some other value. Defaulting to live"
+  full_apps_url+=".apps.dynatrace.com"
+  full_gen2_url=${full_apps_url/.apps./.live.}
+fi
+
 ##############
 # Step 1
 # Inform Dynatrace that a configuration change is occurring
 ##############
 
-curl -X POST "$DT_URL_OBSLAB_LOG_PROBLEM_DETECTION/api/v2/events/ingest" \
+curl -X POST "$full_gen2_url/api/v2/events/ingest" \
   -H "accept: application/json; charset=utf-8" -H "Authorization: Api-Token $DT_API_TOKEN_OBSLAB_LOG_PROBLEM_DETECTION" -H "Content-Type: application/json; charset=utf-8" \
   -d "{
     \"title\": \"feature flag changed\",
