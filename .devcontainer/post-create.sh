5d09a3f72082dcf520d0b3aaa3b63ded5aa37925
Adam Gardner <adam.gardner@dynatrace.com>
2024-09-27 10:58:30 +1000

    update

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,30 +1,64 @@
 #!/bin/bash
 
+#############################################################################
 # install pip packages
 pip install --break-system-packages -r requirements.txt
 
+#############################################################################
 # Install cluster
 kind create cluster --config .devcontainer/kind-cluster.yml --wait 300s
 
+#############################################################################
 # Add Helm chart(s)
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm repo update
 
+#############################################################################
+# Build URL from parts
+full_apps_url="https://${DT_ENV_ID_OBSLAB_LOG_PROBLEM_DETECTION}"
+full_gen2_url=""
+
+if [ "${DT_ENV_OBSLAB_LOG_PROBLEM_DETECTION}" = "dev" ]; then
+  echo "environment is dev"
+  full_apps_url+=".dev.apps.dynatracelabs.com"
+  # Remove apps.
+  full_gen2_url=${full_apps_url/apps.}
+elif [ "${DT_ENV_OBSLAB_LOG_PROBLEM_DETECTION}" = "sprint" ]; then
+  echo "environment is sprint"
+  full_apps_url+=".sprint.apps.dynatracelabs.com"
+  # Remove apps.
+  full_gen2_url=${full_apps_url/apps.}
+else
+  echo "DT_ENV_OBSLAB_LOG_PROBLEM_DETECTION is either 'live' or some other value. Defaulting to live"
+  full_apps_url+=".apps.dynatrace.com"
+  full_gen2_url=${full_apps_url/.apps./.live.}
+fi
+
+#############################################################################
+# Replace placeholders for notebook
+sed -i "s#FULL_APPS_URL_PLACEHOLDER#$full_apps_url#g" otel-demo-values.yaml
+sed -i "s#DOCUMENT_ID_PLACEHOLDER#$DT_NOTEBOOK_ID_LOG_PROBLEM_DETECTION#g" otel-demo-values.yaml
+
+#############################################################################
 # Create DT API secret for collector
 kubectl create secret generic dynatrace-otelcol-dt-api-credentials \
-  --from-literal=DT_ENDPOINT=$DT_URL_OBSLAB_LOG_PROBLEM_DETECTION/api/v2/otlp \
+  --from-literal=DT_ENDPOINT=$full_gen2_url/api/v2/otlp \
   --from-literal=DT_API_TOKEN=$DT_API_TOKEN_OBSLAB_LOG_PROBLEM_DETECTION
 
+#############################################################################
 # Install RBAC items so collector can talk to k8s API
 # to retrieve topology info / pod metadata
 kubectl apply -f collector-rbac.yaml
 
+#############################################################################
 # Install collector
 helm upgrade -i dynatrace-collector open-telemetry/opentelemetry-collector -f collector-values.yaml
 
+#############################################################################
 # Install OpenTelemetry demo app
 helm upgrade -i my-otel-demo open-telemetry/opentelemetry-demo -f otel-demo-values.yaml
 
+#############################################################################
 # Creation Ping
 # curl -X POST https://grzxx1q7wd.execute-api.us-east-1.amazonaws.com/default/codespace-tracker \
 #   -H "Content-Type: application/json" \
--- docs/getting-started.md
+++ docs/getting-started.md
@@ -1,15 +1,47 @@
 # Getting Started
 
-You must have the following to use this hands on demo.
+## Dynatrace Environment
 
-* A Dynatrace environment ([sign up here](https://dt-url.net/trial){target="_blank"})
-* A Dynatrace API token (see below)
+You must have access to a Dynatrace SaaS environment. If you need one, ([sign up here](https://dt-url.net/trial){target="_blank"})
 
-Save the Dynatrace environment URL **without** the trailing slash and without the `.apps.` in the URL:
+Save the Dynatrace environment URL:
 
+* Without the trailing slash
+* Without `.apps.` in the URL
+
+The generic format is:
+
+```
+https://<EnvironmentID>.<Environment>.<URL>
+```
+
+For example:
 ```
 https://abc12345.live.dynatrace.com
 ```
+
+## Custom Runbook
+
+!!! info
+    As the developer responsible for the cartservice, if problems occur, you're the best person to know how to resolve the issue.
+
+    To help your colleagues, you have prebuilt a notebook which will be useful as a runbook if / when problems occur.
+
+    You want to make this notebook automatically available whenever problems with the `cartservice` occur.
+
+Download the file [Redis Troubleshooting.json]() and save to your computer.
+
+In Dynatrace:
+
+* Press `ctrl + k`. Search for `notebooks`
+* Open the app and find the `Upload` button at the top of the page
+* Upload the JSON file you previously downloaded
+
+![upload button](images/notebook-upload-button.png)
+
+* Make a note of the notebook ID from the URL bar
+
+![notebook ID](images/notebook-id.png)
 
 ### Create API Token
 
